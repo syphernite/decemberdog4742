007a9ca949bb46c660a0b182db0e25c1c4fc6049
syphernite <syphernite@gmail.com>
2025-07-18 19:13:13 -0400

    Update App.tsx

--- project/src/thairiverside/App.tsx
+++ project/src/thairiverside/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Menu from './components/Menu';
-import Location from './components/Location';
-import Gallery from './components/Gallery';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Menu from '../components/Menu';
+import Location from '../components/Location';
+import Gallery from '../components/Gallery';
+import Testimonials from '../components/Testimonials';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 function App() {
   return (
