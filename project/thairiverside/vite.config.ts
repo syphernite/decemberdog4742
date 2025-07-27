d01571b9dd1dc0fddcc309c3f4b0e902f386645c
syphernite <syphernite@gmail.com>
2025-07-27 14:02:05 -0400

    Update vite.config.ts

--- project/thairiverside/vite.config.ts
+++ project/thairiverside/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/thairiverside/',
+  base: '/thairiverside/', // for the subfolder site
   plugins: [react()],
 })
