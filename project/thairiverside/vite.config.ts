c68d97c9f012c09a7d4a2942cbe7a053afb190a3
2catcodes <terrillcaden5@gmail.com>
2025-07-27 14:26:56 -0500

    cname and fixed vite.config.ts

--- project/thairiverside/vite.config.ts
+++ project/thairiverside/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/thairiverside/', // for the subfolder site
+  base: '/thairiverside/',
   plugins: [react()],
 })
