4a67384b237f455e6b2928d509509159ae6a2219
syphernite <syphernite@gmail.com>
2025-07-27 14:01:43 -0400

    Update vite.config.ts

--- project/landingpage/vite.config.ts
+++ project/landingpage/vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // for the root site
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
