dab9b8bd7f2aec5c0cd43e97f5f460ed7f977d0a
2catcodes <terrillcaden5@gmail.com>
2025-07-27 18:05:16 -0500

    Update vite.config.ts

--- project/landingpage/vite.config.ts
+++ project/landingpage/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // for the root site
+  base: './',
   plugins: [react()],
 })
+// This configuration sets up a Vite project for a React application
+// with a relative base path and includes the React plugin for Vite.
