8c9049d417f400510ab368257f3e4264ee5bfb09
2catcodes <terrillcaden5@gmail.com>
2025-07-27 18:50:52 -0500

    update vite files

--- project/landingpage/vite.config.ts
+++ project/landingpage/vite.config.ts
@@ -1,9 +1,8 @@
+// project/landingpage/vite.config.js or vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  plugins: [react()],
+  base: '/',
+  plugins: [react()]
 })
-// This configuration sets up a Vite project for a React application
-// with a relative base path and includes the React plugin for Vite.
--- project/thairiverside/vite.config.ts
+++ project/thairiverside/vite.config.ts
@@ -1,9 +1,8 @@
+// project/thairiverside/vite.config.js or vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/thairiverside/',
-  plugins: [react()],
+  plugins: [react()]
 })
-// This configuration sets the base path for the project to '/thairiverside/'
-// and includes the React plugin for Vite to handle React files.
