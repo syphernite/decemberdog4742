64fecda5265848b65e05c10a30dd1e23e4f821a6
2catcodes <terrillcaden5@gmail.com>
2025-07-29 16:37:57 -0500

    debug redirects

--- project/landingpage/vite.config.ts
+++ project/landingpage/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Keep this if deploying to root domain like built4you.org
+  base: '/landingpage/', // or '/' if deployed to root
   build: {
     outDir: 'dist',
   },
