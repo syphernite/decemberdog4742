591c91b70d0df2f9664ab2b05b211af53144b90a
2catcodes <terrillcaden5@gmail.com>
2025-07-30 20:22:11 -0500

    Update vite.config.ts

--- project/coffeeexample/vite.config.ts
+++ project/coffeeexample/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/coffeeexample/',
   plugins: [react()],
+  base: '/coffeeexample/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
