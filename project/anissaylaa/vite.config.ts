80d0eddb0be98840b1ea2620df214e260c5cc6ca
syphernite <syphernite@gmail.com>
2025-08-02 14:06:41 -0400

    Update vite.config.ts

--- project/anissaylaa/vite.config.ts
+++ project/anissaylaa/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/anissaylaa/', // 👈 this line makes your app work at built4you.org/anissaylaa/
+  base: '/anissaylaa/', // ✅ This is CRITICAL
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
