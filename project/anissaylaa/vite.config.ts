894b3664e63148c8ca0d520542ca1faf67c6275a
syphernite <syphernite@gmail.com>
2025-08-02 14:36:32 -0400

    anissaylaa
    Add new

--- project/anissaylaa/vite.config.ts
+++ project/anissaylaa/vite.config.ts
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/anissaylaa/',
+  base: "/anissaylaa/"
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
