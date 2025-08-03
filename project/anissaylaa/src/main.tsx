2494f40737585a8358e7b4ca0df187519b223566
2catcodes <terrillcaden5@gmail.com>
2025-08-02 20:23:06 -0500

    Update main.tsx

--- project/anissaylaa/src/main.tsx
+++ project/anissaylaa/src/main.tsx
@@ -1,8 +1,8 @@
-// main.jsx or main.tsx
+// project/anissaylaa/src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
