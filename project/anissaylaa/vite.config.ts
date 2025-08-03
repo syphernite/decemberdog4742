5b6830dccd8e56ca40b9badc2042588042f79ae8
2catcodes <terrillcaden5@gmail.com>
2025-08-02 20:59:22 -0500

    update annissa

--- project/anissaylaa/vite.config.ts
+++ project/anissaylaa/vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '',
+  base: '/anissaylaa/',
   plugins: [react()],
-  build: {
-    outDir: '../../__deploy/anissaylaa', // <-- only change the folder name here
-    emptyOutDir: true,
-  },
 });
