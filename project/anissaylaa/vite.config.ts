44ab9b5a2dcdbcf01d8d9ec5873250e4340ba4ad
2catcodes <terrillcaden5@gmail.com>
2025-08-02 19:50:15 -0500

    test subdomain routing via vite config update

--- project/anissaylaa/vite.config.ts
+++ project/anissaylaa/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/anissaylaa/',
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    outDir: '../../__deploy/anissaylaa',
+    emptyOutDir: true
+  }
+});
