4ed51c4bf9b48ec713190bca0f71892a850cef69
2catcodes <terrillcaden5@gmail.com>
2025-08-04 09:50:15 -0500

    Update vite.config.ts

--- project/landingpage/vite.config.ts
+++ project/landingpage/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// project/landingpage/vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // ← use relative URLs so built index.html refers to "assets/…" not "/assets/…"
   base: './',
-  plugins: [react()]
-});
+  plugins: [react()],
+})
