2dc0086364367073076914e2ea561aa0181ea3a9
2catcodes <2catcodes@users.noreply.github.com>
2025-08-04 17:35:54 +0000

    deploy: f93721bb6f26ac7873a9bb65f9b9aec26fd91075

--- project/landingpage/src/App.tsx
+++ project/landingpage/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-// Swap BrowserRouter for HashRouter, keep the alias “Router”
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Services from './pages/Services';
@@ -8,9 +8,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
-import WhyWeCreatedBuilt4You from './pages/WhyWeExist'; // ✅ New page import
+import WhyWeCreatedBuilt4You from './pages/WhyWeExist';
 
-// ScrollToTop stays the same
 function ScrollToTopOnRouteChange() {
   const location = useLocation();
   useEffect(() => {
@@ -21,8 +20,7 @@ function ScrollToTopOnRouteChange() {
 
 function App() {
   return (
-    // Only one Router here, now using HashRouter
-    <Router basename="/">
+    <>
       <ScrollToTopOnRouteChange />
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -32,13 +30,10 @@ function App() {
           <Route path="contact" element={<Contact />} />
           <Route path="privacy-policy" element={<PrivacyPolicy />} />
           <Route path="terms-of-service" element={<TermsOfService />} />
-          <Route
-            path="why-we-exist"
-            element={<WhyWeCreatedBuilt4You />}
-          /> {/* ✅ New route */}
+          <Route path="why-we-exist" element={<WhyWeCreatedBuilt4You />} />
         </Route>
       </Routes>
-    </Router>
+    </>
   );
 }
 
