26bef0840511c8b600cc4914bfe1cdba764adef6
2catcodes <terrillcaden5@gmail.com>
2025-08-08 14:56:14 -0500

    fix plumbing

--- project/plumbing/src/App.tsx
+++ project/plumbing/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,21 +10,19 @@ import BackToTop from './components/BackToTop';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-        <BackToTop />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+      <BackToTop />
+    </div>
   );
 }
 
