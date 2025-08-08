93e3ef676e70c6f40ead1a421b1698571a13dae4
2catcodes <terrillcaden5@gmail.com>
2025-08-08 12:08:39 -0500

    fix detailer maybe

--- project/detailer/vite.config.ts
+++ project/detailer/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/detailer/', // change this to match folder name
-})
+  base: '/detailer/',
+  optimizeDeps: { exclude: ['lucide-react'] },
+  build: {
+    rollupOptions: {
+      input: {
+        // MPA entry points
+        main: resolve(__dirname, 'index.html'),
+        services: resolve(__dirname, 'services.html'),
+        results: resolve(__dirname, 'results.html'),
+      },
+    },
+  },
+});
