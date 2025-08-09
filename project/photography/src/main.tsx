8fba08772065bd73ce9ac572eccfde642f51bce4
2catcodes <2catcodes@users.noreply.github.com>
2025-08-09 03:32:35 +0000

    deploy: 34f330abff309fcd918abbef32ea8969bde9ae45

--- project/photography/src/main.tsx
+++ project/photography/src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter basename="/">
-      <HashRouter><App /></HashRouter>
+      <App />
     </HashRouter>
   </StrictMode>
 );
