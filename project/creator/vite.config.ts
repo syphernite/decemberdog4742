90c83eb5fa772df4551223afd6bfad89531f6167
2catcodes <2catcodes@users.noreply.github.com>
2025-08-09 01:30:05 +0000

    deploy: 08da4a6deb8622408f93e29a351c4104444557f5

--- project/creator/vite.config.ts
+++ project/creator/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/creator/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
