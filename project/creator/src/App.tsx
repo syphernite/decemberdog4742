9374cc94db27cb7d88d751d5d9d4b37e7e69118a
2catcodes <2catcodes@users.noreply.github.com>
2025-08-09 04:22:00 +0000

    deploy: 7f28e76271939801df138f651b509901afa476dd

--- project/creator/src/App.tsx
+++ project/creator/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import Content from './pages/Content';
@@ -11,20 +11,18 @@ import MediaKit from './pages/MediaKit';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/content" element={<Content />} />
-          <Route path="/collab" element={<Services />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/booking" element={<Contact />} />
-          <Route path="/media-kit" element={<MediaKit />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/content" element={<Content />} />
+        <Route path="/collab" element={<Services />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/booking" element={<Contact />} />
+        <Route path="/media-kit" element={<MediaKit />} />
+      </Route>
+    </Routes>
   );
 }
 
