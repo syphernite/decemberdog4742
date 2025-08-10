5826240c400810a5ca68dc430697587437844523
2catcodes <2catcodes@users.noreply.github.com>
2025-08-10 18:46:51 +0000

    deploy: 337a30c26eb5897e1e5d70a332945db4d9bdb384

--- project/beachbumz/src/App.tsx
+++ project/beachbumz/src/App.tsx
@@ -1,40 +1,23 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import FloatingOrderButton from './components/FloatingOrderButton';
+import React from "react";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./index.css";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="App">
-        <ScrollToTop />
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <FloatingOrderButton />
+    <HashRouter>
+      <div className="min-h-screen bg-slate-900 text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
-
-export default App;
