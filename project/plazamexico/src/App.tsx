59da9df02bceb9644df8fc2ae6aa18b11b02a33f
2catcodes <terrillcaden5@gmail.com>
2025-09-04 11:14:51 -0500

    finish plaza

--- project/plazamexico/src/App.tsx
+++ project/plazamexico/src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import MenuPage from './pages/MenuPage';
-import ContactPage from './pages/ContactPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import MenuPage from "./pages/MenuPage";
+import ContactPage from "./pages/ContactPage";
+import OurStoryPage from "./pages/OurStoryPage";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
       <Header />
-      <main className="flex-1 pt-20">
+      <main className="flex-1 pt-16">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/our-story" element={<OurStoryPage />} />
         </Routes>
       </main>
       <Footer />
