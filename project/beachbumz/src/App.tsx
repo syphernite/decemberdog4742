9ead7fa0035252698e654d085409139d91a1e988
2catcodes <terrillcaden5@gmail.com>
2025-09-05 09:43:06 -0500

    finish beach bumz

--- project/beachbumz/src/App.tsx
+++ project/beachbumz/src/App.tsx
@@ -3,7 +3,6 @@ import { HashRouter, Routes, Route, Navigate, useLocation } from "react-router-d
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
-import About from "./pages/About";
 import Contact from "./pages/Contact";
 import "./index.css";
 
@@ -22,7 +21,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
