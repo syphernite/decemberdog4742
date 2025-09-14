59306c4c7da365033b2345a8a2b6feeb7fe4d3e6
2catcodes <2catcodes@users.noreply.github.com>
2025-09-14 00:02:42 +0000

    deploy: 8493a7149418440fbc2ccf6faa401600ba40eb6c

--- project/landingpage/src/main.tsx
+++ project/landingpage/src/main.tsx
@@ -1,17 +1,17 @@
-// project/landingpage/src/main.tsx
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <HelmetProvider>
-      <HashRouter basename="/">
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
