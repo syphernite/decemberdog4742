b2b8c5e542cc1df27de595a2ab413eca81716469
2catcodes <terrillcaden5@gmail.com>
2025-09-21 13:02:01 -0500

    Update vite.config.ts

--- project/testsite/vite.config.ts
+++ project/testsite/vite.config.ts
@@ -1,5 +1,10 @@
+// project/testsite/vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import seoPlugin from "./vite.seo";
+
+// adjust base to this folder name
 export default defineConfig({
   plugins: [react(), seoPlugin()],
-  base: "/testsite/"
-})
+  base: "/testsite/",
+});
