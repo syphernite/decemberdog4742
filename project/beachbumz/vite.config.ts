2a85d3f778e4a229fc25122c1ecfd15b40c48bf7
2catcodes <terrillcaden5@gmail.com>
2025-09-21 13:48:22 -0500

    remove beachbumz and add seo

--- project/beachbumz/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import seoPlugin from "./vite.seo";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/beachbumz/",
-  plugins: [react(), seoPlugin()],
-  build: { outDir: "dist", emptyOutDir: true },
-});
