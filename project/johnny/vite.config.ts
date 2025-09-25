098c8bdfe7c5dd18821742c681a6495a66092b0e
2catcodes <terrillcaden5@gmail.com>
2025-09-24 20:11:34 -0500

    fix white screen

--- project/johnny/vite.config.ts
+++ project/johnny/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sb1/',
+  base: '/johnny/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
