3b79b3e9ca90bc5e81f5c80ac1e997a2cbc26a6c
2catcodes <terrillcaden5@gmail.com>
2025-09-25 00:26:44 -0500

    johnny redo

---
+++ project/johnny/src/components/ScrollToTop.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    // Always scroll to top when navigating
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth", block: "start" });
+      } else {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
