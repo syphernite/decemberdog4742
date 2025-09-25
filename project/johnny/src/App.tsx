e6ce2402564dfa64c8df2ca70d805321d8dcf314
2catcodes <2catcodes@users.noreply.github.com>
2025-09-25 01:15:51 +0000

    deploy: 098c8bdfe7c5dd18821742c681a6495a66092b0e

--- project/johnny/src/App.tsx
+++ project/johnny/src/App.tsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/sb1">
+    <Router basename="/johnny">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
