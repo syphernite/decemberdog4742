5e3ff20600a2b3067f0ab9478ba2c2729e00a663
2catcodes <2catcodes@users.noreply.github.com>
2025-10-25 17:38:24 +0000

    deploy: 58c5ff3d219d6b34d5ce0596515b3f6b5c8864cd

--- project/johnny/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import seoPlugin from "./vite.seo";
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), seoPlugin()],
-  base: "/johnny/",
-});
