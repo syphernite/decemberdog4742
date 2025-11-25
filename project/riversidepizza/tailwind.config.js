ba5c06c4b706afdae2de1774efa24db47159a444
2catcodes <2catcodes@users.noreply.github.com>
2025-11-25 17:41:56 +0000

    deploy: 5126351bfd595a29262098c7f2122ccf2307d4f7

--- project/riversidepizza/tailwind.config.js
+++ project/riversidepizza/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '300',
+      },
+    },
   },
   plugins: [],
 };
